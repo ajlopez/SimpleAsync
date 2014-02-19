3ec1495ffb4ff1b8a70afc08b862c43bbae50434
ajlopez <webmaster@ajlopez.com>
2014-02-19 07:40:18 -0300

    Async step test

--- test/then.js
+++ test/then.js
@@ -18,7 +18,7 @@ exports['then one step'] = function (test) {
     test.strictEqual(seq.run(10), seq);
 }
 
-exports['then two step'] = function (test) {
+exports['then with two sync steps'] = function (test) {
     test.async();
     
     var total = 0;
@@ -27,6 +27,29 @@ exports['then two step'] = function (test) {
         .then(function (data) {
             total += data;
             return data;
+        })
+        .then(function (data) {
+            total += data;
+            
+            test.equal(total, 12);
+            test.done();
+        });
+    
+    test.ok(seq);
+    test.strictEqual(seq.run(6), seq);
+}
+
+exports['then with one async step and one sync step'] = function (test) {
+    test.async();
+    
+    var total = 0;
+    
+    var seq = async()
+        .then(function (data, next) {
+            total += data;
+            setImmediate(function () {
+                next(data);
+            });
         })
         .then(function (data) {
             total += data;
