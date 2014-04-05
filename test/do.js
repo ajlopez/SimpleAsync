1b221e08214951b799d014389b295ffb272b6ab3
ajlopez <webmaster@ajlopez.com>
2014-04-05 07:06:37 -0300

    do over sync functions

---
+++ test/do.js
@@ -0,0 +1,24 @@
+
+var async = require('..');
+
+exports['do two functions'] = function (test) {
+    test.async();
+    
+    var total = 0;
+    
+    var seq = async()
+        .do(function (data) { return data + 1; })
+        .do(function (data) { return data + 2; })
+        .then(function (data) {
+            test.ok(data);
+            test.ok(Array.isArray(data));
+            test.equal(data.length, 2);
+            test.equal(data[0], 2);
+            test.equal(data[1], 3);
+            test.done();
+        });
+    
+    test.ok(seq);
+    test.strictEqual(seq.run(1), seq);
+}
+
