4a028fee8f3b75ea8e21e76eb458a03a2c71afb4
ajlopez <webmaster@ajlopez.com>
2014-04-09 19:51:30 -0300

    Map with callback function, WIP

--- test/map.js
+++ test/map.js
@@ -1,7 +1,7 @@
 
 var async = require('..');
 
-exports['then one step'] = function (test) {
+exports['map and then'] = function (test) {
     test.async();
     
     var total = 0;
@@ -9,6 +9,30 @@ exports['then one step'] = function (test) {
     var seq = async()
         .map(function (item) {
             return item * item;
+        })
+        .then(function (data) {
+            test.ok(data);
+            test.ok(Array.isArray(data));
+            test.equal(data.length, 3);
+            test.equal(data[0], 1);
+            test.equal(data[1], 4);
+            test.equal(data[2], 9);
+
+            test.done();
+        });
+    
+    test.ok(seq);
+    test.strictEqual(seq.run([1,2,3]), seq);
+}
+
+exports['map with async fn and then'] = function (test) {
+    test.async();
+    
+    var total = 0;
+    
+    var seq = async()
+        .map(function (item, next) {
+            next(null, item * item);
         })
         .then(function (data) {
             test.ok(data);
