cc93ae7f990c64723c413543bcfd67bbe823fd42
ajlopez <webmaster@ajlopez.com>
2014-08-23 19:27:17 -0300

    Fix map async with empty array

--- test/map.js
+++ test/map.js
@@ -48,3 +48,24 @@ exports['map with async fn and then'] = function (test) {
     test.ok(seq);
     test.strictEqual(seq.run([1,2,3]), seq);
 }
+
+exports['map empty array with async fn and then'] = function (test) {
+    test.async();
+    
+    var total = 0;
+    
+    var seq = async()
+        .map(function (item, next) {
+            next(null, item * item);
+        })
+        .then(function (data) {
+            test.ok(data);
+            test.ok(Array.isArray(data));
+            test.equal(data.length, 0);
+
+            test.done();
+        });
+    
+    test.ok(seq);
+    test.strictEqual(seq.run([]), seq);
+}
