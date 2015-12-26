c0bc0d3eff0365a51e306a781e545f6c5a8fa61f
ajlopez <webmaster@ajlopez.com>
2015-12-26 11:14:18 -0300

    Removing support for do sync functions

--- test/deprecated/do.js
+++ test/deprecated/do.js
@@ -7,8 +7,8 @@ exports['do two functions'] = function (test) {
     var total = 0;
     
     var seq = async()
-        .do(function (data) { return data + 1; })
-        .do(function (data) { return data + 2; })
+        .do(function (data, next) { next(null, data + 1); })
+        .do(function (data, next) { next(null, data + 2); })
         .then(function (data) {
             test.ok(data);
             test.ok(Array.isArray(data));
@@ -28,8 +28,8 @@ exports['do two functions with fail'] = function (test) {
     var total = 0;
     
     var seq = async()
-        .do(function () { return 1; })
-        .do(function () { return 2; })
+        .do(function (data, next) { next(null, 1); })
+        .do(function (data, next) { next(null, 2); })
         .then(function (data) {
             test.ok(data);
             test.ok(Array.isArray(data));
@@ -43,27 +43,6 @@ exports['do two functions with fail'] = function (test) {
         });
     
     test.ok(seq);
-}
-
-exports['do two async functions with explicit run'] = function (test) {
-    test.async();
-    
-    var total = 0;
-    
-    var seq = async()
-        .do(function (data, next) { next(null, data + 1); })
-        .do(function (data, next) { next(null, data + 2); })
-        .then(function (data) {
-            test.ok(data);
-            test.ok(Array.isArray(data));
-            test.equal(data.length, 2);
-            test.equal(data[0], 2);
-            test.equal(data[1], 3);
-            test.done();
-        });
-    
-    test.ok(seq);
-    test.strictEqual(seq.run(1), seq);
 }
 
 exports['do two async functions with run and with error'] = function (test) {
